12bfd69a1c9d1c9da7856a61fc99bbef5997d3c5
Ethan <ethy2010nz@gmail.com>
2026-03-15 13:06:03 +1300

    At home coding

--- code.py
+++ code.py
@@ -1,1 +1,20 @@
-print("hello")
+#List of the chooseable activeties
+#Activtys can be changed here
+name_input = ""
+age_input = ""
+camp_input =""
+meal_input =""
+shuttle_input =""
+activity_list = [("1","Cultural immersion",5,"easy ",800)]
+activity_list1 = [("2","Kayaking & pancakes",3,"moderate" ,400)]
+activity_list2 = [("3","Mountain biking",4,"difficult", 900)]
+print("  num   act                days  dif    cost")
+print(f"{activity_list}")
+print(f"{activity_list1}")
+print(f"{activity_list2}")
+input("what is your name? ")
+camper_age =""
+while camper_age == "" or camper_age.isdigit() == False :
+    camper_age = input("what is your age? ")
+    if camper_age  == ""or camper_age.isdigit() == False: print("you need to enter your age")
+if int(camper_age) > 4 and int(camper_age) < 18:
